4f74198ac797f8562328fab0221c33e0cf6ea865
Patrick Little <little.patrick@outlook.com>
2025-01-22 21:44:31 -0700

    first commit

---
+++ src/password_manager/enigma.py
@@ -0,0 +1,18 @@
+
+
+class Enigma
+    def __intit__(self, password, key1, key2):
+        self.password = password
+        self.key1 = key1
+        self.key2 = key2
+
+    def encrypt(self):
+        shift
+
+    def decrupt(self):j
+        reverse_shift
+
+    def __shift(self):
+
+       for letter in self.key1:
+       
---
+++ tests/test_enigma.py
@@ -0,0 +1,5 @@
+from enigma inport Enigma
+
+def test_key_shift()
+    enigma = Enigma(Blanch, "123456", "123456")
+    
