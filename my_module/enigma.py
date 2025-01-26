68ba58e61e975463d82649e0e303fe789229ed2d
Patrick Little <little.patrick@outlook.com>
2025-01-25 20:48:53 -0700

    and that's pretty cool

--- my_module/enigma.py
+++ my_module/enigma.py
@@ -1,27 +1,57 @@
 import ipdb
-class Enigma:
-    def __init__(self, password, key1, key2):
+import string
+    def __init__(self, password, key, date):
         self.password = password
-        self.key1 = key1
-        self.key2 = key2
+        self.key = key
+        self.date = date
 
     def encrypt(self):
-        return self.shift()
+       pass
        
     def decrypt(self):
-        reverseShift()
+        reverse_shift()
 
-    def shift(self):
-        splitKey = list(self.key1)
-        aKey = ''.join(splitKey[0:2])
-        bKey = ''.join(splitKey[1:3])
-        cKey = ''.join(splitKey[2:4])
-        dKey = ''.join(splitKey[3:5])
-        
-        ipdb.set_trace()
+    def shift_key(self):
+        split_key = list(self.key)
+        key_offset = {
+            "a": ''.join(split_key[0:2]),
+            "b": ''.join(split_key[1:3]),
+            "c": ''.join(split_key[2:4]),
+            "d": ''.join(split_key[3:5])
+        }
+        return key_offset
 
-        return a
+    def shift_date(self):
+        square = int(self.date) ** 2
+        split_date = list(str(square))
+        date_offset = {
+            "a": ''.join(split_date[-1]),
+            "b": ''.join(split_date[-2]),
+            "c": ''.join(split_date[-3]), 
+            "d": ''.join(split_date[-4])
+        }
+        return date_offset
 
-    def reverseShift(self):
+    def final_key(self):
+        final = {}
+        for key in self.shift_date():
+            final[key] = int(self.shift_date()[key]) + int(self.shift_key()[key])
+        return final
+
+    def rotate(self, l, n):
+        n = n % len(lst)
+        return l[n:] + l[:n]
+
+    def offest(self):
+       hash_letter_key = {}
+       letter_key = self.letters()
+       for index, value in enumerate(letter_key):
+            hash_letter_key[value] = value
+       ipdb.set_trace()
+
+    def reverse_shift(self):
         pass
 
+    def letters(self):
+        lowercase = list(string.ascii_lowercase)
+        return lowercase
--- tests/test_enigma.py
+++ tests/test_enigma.py
@@ -6,8 +6,23 @@ from my_module.enigma import Enigma
 def enigma():
     return Enigma("jackson", "12345", "123456")
 
-def test_shift(enigma):
-    assert enigma.encrypt() == "12"
+def test_shift_key(enigma):
+    key_shift = {
+        "a": "12",
+        "b": "23",
+        "c": "34",
+        "d": "45"
+    }
+    assert enigma.shift_key() == key_shift
+
+def test_shift_date(enigma):
+    assert enigma.shift_date() == {'a': '6', 'b': '3', 'c': '9', 'd': '3'}
+    
+def test_final_key(enigma):
+    assert enigma.final_key() == {'a': 18, 'b': 26, 'c': 43, 'd': 48}
+
+def test_offset(enigma):
+    assert enigma.offest() == {}
 
 if __name__ == "__main__":
     pytest.main()
